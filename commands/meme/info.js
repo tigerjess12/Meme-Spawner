d42d6ad9f5e1b929c9fea2ef4dcd3c33258b1ae2
tigerjess12 <71166173+tigerjess12@users.noreply.github.com>
2025-05-05 14:54:30 +1200

    Commit

--- commands/meme/info.js
+++ commands/meme/info.js
@@ -55,6 +55,26 @@ module.exports = {
                   value: "This bot is licensed under the GNU GPL v3. See the LICENSE file for details.",
                   inline: false
                 },
+                {
+                  name: "Disclaimer",
+                  value: `\`\`\`
+Meme Spawner Bot - discord bot for sharing memes
+Copyright (C) 2025  tigerjess12
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+\`\`\``,
+                  inline: false
+                }
               )
               .setThumbnail("https://avatars.githubusercontent.com/u/71166173?v=4")
               .setColor("#f50000")
